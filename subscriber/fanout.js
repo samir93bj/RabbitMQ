0690b2d21b075e68df6af4e5dd8dd0d9e8ce58b6
samir mahmud <samir93bj@gmail.com>
2023-03-30 13:17:50 -0300

    feat: add config and example fanout structure

---
+++ subscriber/fanout.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const amqp = require('amqplib')
+const queue = process.env.QUEUE || 'hello'
+const exchangeName = process.env.QUEUE || 'my-fanout'
+
+console.log({
+	queue,
+	exchangeName
+})
+
+function intensiveOperation() {
+    let i = 1e3
+    while (i--) {}
+}
+
+async function subscriber() {
+    const connection = await amqp.connect('amqp://localhost')
+    const channel = await connection.createChannel()
+
+    await channel.assertQueue(queue)
+		await channel.bindQueue(queue, exchangeName)
+
+    channel.consume(queue, (message) => {
+      const content = JSON.parse(message.content.toString())
+
+      intensiveOperation()
+
+      console.log(`Received message from "${queue}" queue`)
+      console.log(content)
+
+      channel.ack(message)
+    })
+}
+
+subscriber().catch((error) => {
+    console.error(error)
+    process.exit(1)
+})
